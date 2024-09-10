ac4aacde4b7e1d3d7cb1db3bd9eacd760f953d98
Ifartedatmyroomatesdoor <bendojews@gmail.com>
2024-09-10 20:48:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Import Firebase SDK functions (make sure you're using Firebase v9 syntax)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDmEc-So0p1YAyL4IAXJfykxT1FpveWZQk",
     authDomain: "danii2-s-archive.firebaseapp.com",
